d2a1be2f42a0e723adb8d5c35c8501b70be008d2
naelrouz <nael_rouz@mail.ru>
2016-05-17 15:17:24 +0300

    HW #1.6

--- HomeWork/1/1.6.php
+++ HomeWork/1/1.6.php
@@ -5,33 +5,36 @@
  * Time: 14:26
  */ ?>
 
-    <h4>
+
+
+<?php
+echo '<h4>
         6. Рекурсивную функцию, принимающую два целых числа начальное
         значение и
         конечное значение. Например, первый аргумент 10, второй 35.
         Функция должна
         вывести на список нечетных чисел от 10 до 35.
-    </h4>
+    </h4>';
 
-<?php
 
-//echo recursion(5,10);
+echo recursion(5,35);
 function recursion($begin, $end)
 {
     $counter = $begin;
-    if ($counter < $end) {
-        echo ($counter++) . ' | ';
-        callself($counter);
+    if ($counter <= $end) {
+        echo ($counter%2) ? $counter . ' | ' : '';
+        $counter++;
+        recursion($counter,$end);
     } else {
         return $counter;
     }
 }
 
-// Из примера
-echo callself(5); echo '<br> Из примера';
+echo '<p> Из примера </p> '; // Из примера
+echo callself(5);
 function callself($counter){
     if ($counter > 0) {
-        echo ($counter++) . ' | ';
+        echo ($counter--) . ' | ';
         callself($counter);
     } else {
         return $counter;
